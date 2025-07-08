f0e98de7426fe7f10964403e5216c20f5a0f5e85
Faleye Tolulope Janet <67978592+tolulopejanet@users.noreply.github.com>
2025-07-08 22:28:58 +0000

    first

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+    <p className='text-3xl'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi tenetur atque quam delectus obcaecati id necessitatibus? Odio vero cupiditate voluptates asperiores laboriosam omnis dolorum officiis, mollitia dolores, quam nulla error.</p>
+    </>
+  )
+}
+
+export default App
